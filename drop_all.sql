33dbdb11919d737fac0fe7c1f0463d620ac6a1ed
Nicolas Tosco <tosconicolas@gmail.com>
2017-05-05 23:17:08 -0300

    Agrego tabla Usuario y RolxUsuario

--- drop_all.sql
+++ drop_all.sql
@@ -1,3 +1,5 @@
 drop table [GARBAGE].FuncionalidadxRol
 drop table [GARBAGE].Funcionalidad
 drop table [GARBAGE].Rol
+drop table [GARBAGE].Usuario
+drop table [GARBAGE].RolxUsuario
--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -60,3 +60,54 @@ create table [GARBAGE].FuncionalidadxRol(
 	primary key(func_rol_rol_id, func_rol_func_id)
 )
 go
+
+/**************************************
+			USUARIO
+**************************************/
+
+-- Tener en cuenta que se debe encriptar la contraseña como SHA256
+-- Agregar el usuario "admin" y pass "w23e"
+-- perfil Administrador
+
+CREATE TABLE [GARBAGE].Usuario(
+	usu_id numeric(18,0) primary key identity (0,1),
+	usu_username nvarchar(255) unique NOT NULL,
+	usu_password nvarchar(255) NOT NULL,
+	usu_activo bit default 1 NOT NULL,
+	usu_intentos numeric(1,0) default 0 NOT NULL)
+GO
+
+-- Buscar cual seria la contrasela w23e en este hash
+
+DECLARE @hash binary(32)
+SET @hash =  CONVERT(binary(32),'0xe6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7',1)
+
+INSERT INTO [GARBAGE].Usuario(usu_username,usu_password)
+VALUES
+	('admin', @hash),
+	('david', @hash),
+	('fede', @hash),
+	('nico', @hash),
+	('fer', @hash)
+GO
+
+/**************************************
+			ROL POR USUARIO
+**************************************/
+
+create table [GARBAGE].RolxUsuario(
+	rol_usu_rol_id numeric(18,0) foreign key references [GARBAGE].Rol(rol_id) NOT NULL,
+	rol_usu_usu_id numeric(18,0) foreign key references [GARBAGE].Usuario(usu_id) NOT NULL
+	primary key(rol_usu_rol_id, rol_usu_usu_id)
+)
+go
+
+INSERT INTO [GARBAGE].RolxUsuario(rol_usu_rol_id , rol_usu_usu_id)
+(
+	SELECT R.rol_id , U.usu_id
+	FROM [GARBAGE].Usuario S 
+	JOIN [GARBAGE].Rol R ON S.usu_username = 'admin' AND R.rol_nombre = 'Administrador'
+)
+GO
+
+-- NOTA: Probar que ande 
