2aac4a37d2d16fa1820e9ddaa6ef34384b59ba79
Nicolas Tosco <tosconicolas@gmail.com>
2017-05-30 01:22:04 -0300

    Comienzo con aplicacion agregando forms

--- UberFrba/Abm Automovil/MenuAutomovil.cs
+++ UberFrba/Abm Automovil/MenuAutomovil.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace UberFrba.Abm_Automovil
 {
-    public partial class Form1 : Form
+    public partial class MenuAutomovil : Form
     {
-        public Form1()
+        public MenuAutomovil()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AltaAutomovil formAlta = new AltaAutomovil();
+            formAlta.ShowDialog(); 
         }
     }
 }
