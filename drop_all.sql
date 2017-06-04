ca3312c67a32d4ff8bfd20115bf6875a65a514de
David <daver1419@gmail.com>
2017-06-04 19:49:48 -0300

    reformat drop_all

--- drop_all.sql
+++ drop_all.sql
@@ -10,32 +10,21 @@ alter table GARBAGE.ChoferxAutomovil drop constraint FK_chof_auto_chof_id;
 alter table GARBAGE.ChoferxAutomovil drop constraint FK_chof_auto_auto_id;
 alter table GARBAGE.TurnoxAutomovil drop constraint FK_turno_auto_turno_id;
 alter table GARBAGE.TurnoxAutomovil drop constraint FK_turno_auto_auto_id;
-
-alter table GARBAGE.Factura drop constraint FK_fact_cli_id;
-
 alter table GARBAGE.Viaje drop constraint FK_viaje_auto_id;
 alter table GARBAGE.Viaje drop constraint FK_viaje_turno_id;
 alter table GARBAGE.Viaje drop constraint FK_viaje_chof_id;
 alter table GARBAGE.Viaje drop constraint FK_viaje_cli_id;
-
-alter table GARBAGE.ItemxFactura drop constraint FK_fact_fact_id;
-alter table GARBAGE.ItemxFactura drop constraint FK_fact_viaje_id;
-
 alter table GARBAGE.Rendicion drop constraint FK_rend_chofer_id;
 alter table GARBAGE.Rendicion drop constraint FK_rend_turno_id;
+alter table GARBAGE.ItemxFactura drop constraint FK_fact_fact_id;
+alter table GARBAGE.ItemxFactura drop constraint FK_fact_viaje_id;
+alter table GARBAGE.Factura drop constraint FK_fact_cli_id;
 go
 
 drop table GARBAGE.FuncionalidadxRol
 drop table GARBAGE.Funcionalidad
 drop table GARBAGE.RolxUsuario
 drop table GARBAGE.Rol
-
-drop table GARBAGE.ItemxFactura
-drop table GARBAGE.Factura
-drop table GARBAGE.Viaje
-
-
-
 drop table GARBAGE.Cliente
 drop table GARBAGE.Usuario
 drop table GARBAGE.Chofer
@@ -45,8 +34,10 @@ drop table GARBAGE.Automovil
 drop table GARBAGE.ChoferxAutomovil
 drop table GARBAGE.Turno
 drop table GARBAGE.TurnoxAutomovil
+drop table GARBAGE.Viaje
 drop table GARBAGE.Rendicion
-
+drop table GARBAGE.ItemxFactura
+drop table GARBAGE.Factura
 
 drop function GARBAGE.GenerarUsuario;
 drop function GARBAGE.RemoverTildes;
